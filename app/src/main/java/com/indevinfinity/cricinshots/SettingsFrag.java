dbc9ae0fd7ca5762a0523a7cfab74fd2b354091d
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-07-10 12:54:22 +0530

    fixes SettingsFrag.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/SettingsFrag.java
@@ -0,0 +1,51 @@
+package com.indevinfinity.cricinshots;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFrag extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private Context mContext;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public SettingsFrag() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static SettingsFrag newInstance(String param1, String param2) {
+        SettingsFrag fragment = new SettingsFrag();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //final View sf = inflater.inflate(R.layout.fragment_settings, container, false);
+        final View sf = inflater.inflate(R.layout.fragment_settings_light, container, false);
+
+
+        return sf;
+    }
+}
