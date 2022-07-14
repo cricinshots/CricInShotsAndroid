3da3152f15a4dac1dd487902404b592315f56b5d
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-07-14 12:54:22 +0530

    fixes guess.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/guess.java
@@ -0,0 +1,34 @@
+package com.indevinfinity.cricinshots;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class guess extends Fragment {
+
+
+    public guess() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_guess, container, false);
+
+        // Do all the stuff to initialize your custom view
+
+        return v;
+    }
+
+}
