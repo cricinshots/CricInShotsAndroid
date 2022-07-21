726860e29e2da1f21e76aaca5200048ef0f3bdff
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-07-21 12:54:22 +0530

    add overballdata.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/overballdata.java
@@ -0,0 +1,12 @@
+package com.indevinfinity.cricinshots;
+
+public class overballdata {
+    int Id;
+    String Toshow;
+    public overballdata(int id, String toshow){
+        this.Id = id;
+        this.Toshow = toshow;
+    }
+
+
+}
