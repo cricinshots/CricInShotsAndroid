f1bd9ea61dab659be91231f42a133448af780fd1
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-08-09 12:54:22 +0530

    add playersdata.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/playersdata.java
@@ -0,0 +1,39 @@
+package com.indevinfinity.cricinshots;
+
+public class playersdata {
+    String battingStyle;
+    String bio;
+    String bowlingStyle;
+    String didYouKnow;
+    String dob;
+    String firstName;
+    String fullName;
+    String height;
+    String imageURL;
+    String lastName;
+    String odiDebutDate;
+    long playerId;
+    String playerType;
+    String t20DebutDate;
+    String testDebutDate;
+
+public playersdata(String battingStyle,String bio, String bowlingStyle,String didYouKnow,String dob,String firstName,String fullName,String height,String imageURL, String lastName, String odiDebutDate, long playerId, String playerType, String t20DebutDate, String testDebutDate){
+
+    this.battingStyle = battingStyle;
+    this.bio = bio;
+    this.bowlingStyle = bowlingStyle;
+    this.didYouKnow = didYouKnow;
+    this.dob = dob;
+    this.firstName = firstName;
+    this.fullName = fullName;
+    this.height = height;
+    this.imageURL = imageURL;
+    this.lastName = lastName;
+    this.odiDebutDate = odiDebutDate;
+    this.playerId = playerId;
+    this.playerType = playerType;
+    this.t20DebutDate = t20DebutDate;
+    this.testDebutDate = testDebutDate;
+
+}
+}
