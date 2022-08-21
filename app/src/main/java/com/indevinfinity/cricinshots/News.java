f2f8da0d9e761a6f0248de1808b9b68c5b769907
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-08-21 12:54:22 +0530

    update News.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/News.java
@@ -0,0 +1,67 @@
+package com.indevinfinity.cricinshots;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class News extends Fragment {
+    WebView wv1;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public News() {
+        // Required empty public constructor
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NotificationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static News newInstance(String param1, String param2) {
+        News fragment = new News();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View nv = inflater.inflate(R.layout.fragment_news, container, false);
+        wv1 = nv.findViewById(R.id.fragwv);
+        WebSettings webSettings = wv1.getSettings();
+        wv1.setWebViewClient(new WebViewClient());
+        webSettings.setJavaScriptEnabled(true);
+        wv1.loadUrl("https://cricinshots.com/news/");
+        return nv;
+    }
+}
