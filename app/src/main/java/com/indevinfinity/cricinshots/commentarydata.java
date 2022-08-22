24e4db6a3d63cae69beb45126662894d75cb477b
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-08-22 12:54:22 +0530

    update commentarydata.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/commentarydata.java
@@ -0,0 +1,17 @@
+package com.indevinfinity.cricinshots;
+
+public class commentarydata {
+
+
+    String Text;
+    String Head;
+
+    public commentarydata(String text, String head) {
+
+        this.Text = text;
+        this.Head = head;
+
+
+    }
+
+}
