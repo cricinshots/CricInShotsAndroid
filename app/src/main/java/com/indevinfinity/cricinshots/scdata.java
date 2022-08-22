2411d891a96d14ea58b3f9ba9ddb123343e15c2a
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2022-08-22 12:54:22 +0530

    update scdata.java

---
+++ app/src/main/java/com/indevinfinity/cricinshots/scdata.java
@@ -0,0 +1,13 @@
+package com.indevinfinity.cricinshots;
+
+public class scdata {
+    String Title;
+    String Content;
+
+    public scdata(String title, String content){
+
+        this.Title = title;
+        this.Content = content;
+
+    }
+}
